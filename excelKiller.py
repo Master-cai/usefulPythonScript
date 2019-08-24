371f8bde394e5b728fd245cc336d00a2139a014d
Master-cai <719591339@qq.com>
2019-08-24 22:50:57 +0800

    add CLI and prompt message

--- excelKiller.py
+++ excelKiller.py
@@ -4,6 +4,7 @@ import xlrd
 import xlwt
 import re
 from xlutils.copy import copy
+import argparse
 
 
 def getlist(workSpaceDir):
@@ -22,12 +23,9 @@ def get_row_data(fileList):
         name_true = file.split('.')[0]
         data = xlrd.open_workbook(file)
         table = data.sheets()[0]
-        # print(table.nrows)
         for i in range(table.nrows):
             name = table.row(i)[4].value
             if name == name_true:
-                # print(name)
-                # print(table.row(i))
                 data_list.append(table.row(i))
     return data_list
 
@@ -52,18 +50,28 @@ def write_rows(rowsData, outPut, oldRowNum):
     style.borders = borders
     i = oldRowNum
     for row in rowsData:
-        print(row)
         for j in range(len(row)):
-            print(row[j].value)
             workSheet.write(i, j, str(row[j].value), style)
+        print('file {}.xls has been merged'.format(row[4].value))
         i += 1
     workBook.save(outPut)
     return 'successful !'
 
 
+def addarg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-workPath', type=str, help='the path of your excels', default=os.getcwd())
+    parser.add_argument('-oldRowNum', type=str, help='the row number you want to begin', default=2)
+    parser.add_argument('-outPutFiles', type=str, help='the file you want ro save the output, it must exist',
+                        default='output.xls')
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == '__main__':
-    path = os.getcwd()
+    args = addarg()
+    output = args.outPutFiles
+    path = args.workPath
     fileList = getlist(path)
     rows = get_row_data(fileList)
-    output = 'output.xls'
-    print(write_rows(rows, output, 2))
+    print(write_rows(rows, output, args.oldRowNum))
